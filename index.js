3d70a09b633f1f8ea9f9fd56b8f8e2edbb8a03b5
oabdulhadi <abdulhadiosama@gmail.com>
2023-01-02 17:46:05 +0300

    version1

--- index.js
+++ index.js
@@ -3,44 +3,80 @@
     
 // });
 
-var numberOfItems = document.querySelectorAll('.drum').length;
 
 
-for (i=0; i<=numberOfItems; i++)
+document.addEventListener('keydown', (event) => {
+
+    // console.log(this.event);
+    //alert(this.event.key);
+    eventListener(this.event.key);
+    // playAnimation(this.event.key);
+    console.log(this.event.key);
+    playAnimation(this.event.key);
+});
+
+
+function eventListener(keyCode)
 {
+    var buttonInnerHTML = this.innerHTML;
+   // console(this.keyCode);
 
-    document.querySelectorAll("button")[i].addEventListener("click", eventListener);
+    switch (keyCode) {
+        case "w":
+            playSound("crash");
+            break;
+
+        case "a":
+            playSound("kick-bass");
+            break;
+    
+        case "s":
+            playSound("snare");  
+            break;
+
+        case "d":
+            playSound("tom-1");
+            break;
+
+        case "d":
+            playSound("tom-2");
+            break;
+
+        case "j":
+            playSound("tom-3");
+            break;
+
+        case "k":
+            playSound("tom-4");
+            break;
+
+        case "l":
+            playSound("tom-4");
+            break;
+
+    default:
+        break;
+   } 
 }
 
-function eventListener()
+
+function playSound(soundName)
 {
-    alert("I got clicked");
+    var soundToPlay = "sounds/" + soundName + ".mp3";
+    var audio = new Audio(soundToPlay);
+    audio.play();
+    
+
+
 }
 
-
-
-function add(num1, num2)
+function playAnimation(curentKey)
 {
-    return num1+num2;
-}
+   // console.log(currentKey);
+     var activeButton =    document.querySelector("." + curentKey);
+    activeButton.classList.add("pressed");
 
-function mult(num1,num2)
-{
-    return num1*num2;
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100);
 }
-
-function divide(num1,num2)
-{
-    return num1/num2;
-}
-
-function subtract(num1,num2)
-{
-    return num1-num2;
-}
-
-function calculator(num1, num2, operator)
-{
-    return operator(num1,num2);
-}
-
